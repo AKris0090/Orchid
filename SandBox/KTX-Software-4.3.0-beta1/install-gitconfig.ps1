c839b700f7958fb2b8362567cd315b79497e76c9
Arjun Krishnan <akrish29@ucsc.edu>
2024-09-18 16:03:50 -0700

    useless files

--- SandBox/KTX-Software-4.3.0-beta1/install-gitconfig.ps1
+++
@@ -1,8 +0,0 @@
-# Copyright 2023 The Khronos Group Inc.
-# SPDX-License-Identifier: Apache-2.0
-
-# Add [include] of repo's .gitconfig in clone's .git/config.
-# This only needs to be run once.
-
-git config --local include.path ..\.gitconfig
-echo 'Git config was successfully set.'
